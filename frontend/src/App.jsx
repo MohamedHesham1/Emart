cd83371fe85892a51cee65815354e0f4ef7802f8
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-04-06 07:00:23 +0200

    feat: :sparkles: add Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
 import Header from './components/Header';
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
