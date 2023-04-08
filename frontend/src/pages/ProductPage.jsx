cfd2f341e9cb3ff231e0987c95cf6ef2fb73814f
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-04-08 09:24:04 +0200

    feat: :sparkles: fetch the products from the backend

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import {
-  Row,
+  Button,
+  Card,
   Col,
   Image,
   ListGroup,
-  Card,
-  Button,
   ListGroupItem,
+  Row,
 } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
-import products from '../products';
 
 function ProductPage() {
   const params = useParams();
-  const product = products.find((p) => p._id === params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`);
+
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, []);
 
   return (
     <>
