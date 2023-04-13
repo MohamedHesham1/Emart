54067c03b35210b964797b75794b1a058da7062e
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-04-13 08:11:57 +0200

    refactor: :recycle: change state naming

--- frontend/src/features/productsListSlice.js
+++ frontend/src/features/productsListSlice.js
@@ -1,27 +1,21 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const initialState = {
-  products: [],
-  loading: false,
-  error: '',
-};
-
 export const fetchProducts = createAsyncThunk(
   '/api/products',
-  async (arg, thunkAPI) => {
+  async (arg, { rejectWithValue }) => {
     try {
       const response = await axios.get('/api/products');
       return response.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      throw rejectWithValue(error.message);
     }
   }
 );
 
 export const productListSlice = createSlice({
   name: 'productList',
-  initialState,
+  initialState: { products: [], loading: false, httpError: '' },
   extraReducers: (builder) => {
     builder.addCase(fetchProducts.pending, (state) => {
       state.loading = true;
@@ -29,15 +23,14 @@ export const productListSlice = createSlice({
     builder.addCase(fetchProducts.fulfilled, (state, action) => {
       state.loading = false;
       state.products = action.payload;
-      state.error = '';
+      state.httpError = '';
     });
     builder.addCase(fetchProducts.rejected, (state, action) => {
       state.loading = false;
       state.products = action.payload;
-      state.error = action.error.message;
+      state.httpError = action.payload;
     });
   },
 });
 
 export default productListSlice.reducer;
-export const { request, success, error } = productListSlice.actions;
