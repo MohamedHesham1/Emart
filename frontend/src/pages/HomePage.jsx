6673d6f22e68c57f61494083d8f8a1799a7a7608
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-04-13 08:13:29 +0200

    feat: :sparkles: add Loader and Message components to HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import Product from '../components/Product';
-import { fetchProducts } from '../features/productsSlice';
+import { fetchProducts } from '../features/productsListSlice';
 
 function HomePage() {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const { loading, httpError, products } = productList;
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -18,9 +20,9 @@ function HomePage() {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
-      ) : error ? (
-        <h3>{error}</h3>
+        <Loader />
+      ) : httpError ? (
+        <Message variant='danger'>{httpError}</Message>
       ) : (
         <Row>
           {products.map((product) => (
