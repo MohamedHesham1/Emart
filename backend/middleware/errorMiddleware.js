ba0495bf5c862f2bd3b67cb8ef7dbe25a0b5785a
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-06 17:58:00 +0300

    fix: :bug: correct custom error message not working properly

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,11 +5,17 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    message = 'Resource not found';
+    statusCode = 404;
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'production' ? 'production' : err.stack,
   });
 };
 
