a264e7fb197928d066f265a9c015d0cbb080bff1
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-06 17:56:07 +0300

    fix: :bug: add missing fields to DB schemas

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,19 +3,21 @@ import bcrypt from 'bcryptjs';
 const users = [
   {
     name: 'Admin User',
-    email: 'admin@example.com',
-    password: bcrypt.hashSync('123456', 10),
+    email: 'mohamed.hesham.2049@gmail.com',
+    password: bcrypt.hashSync('01124100241h', 10),
     isAdmin: true,
   },
   {
     name: 'John Doe',
     email: 'john@example.com',
     password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
   },
   {
     name: 'Jane Doe',
     email: 'Jabe@example.com',
     password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
   },
 ];
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 connectDB();
@@ -18,8 +19,6 @@ app.use('/api/products', productRoutes);
 app.use(notFound);
 
 app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
