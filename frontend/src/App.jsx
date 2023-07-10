1d989f209e7179338a839941e596342c858ccdb7
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-10 21:38:56 +0300

    refactor: :recycle: move routes to main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
-import CartPage from './pages/CartPage';
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <main className='py-3'>
         <Container>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/product/:id' element={<ProductPage />} />
-            <Route path='/cart/:id?' element={<CartPage />} />
-          </Routes>
+          <Outlet />
         </Container>
       </main>
       <Footer />
-    </Router>
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App';
 import store from './app/store';
-import './bootstrap.min.css';
-import './index.css';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomePage />} />
+      <Route path='/product/:id' element={<ProductPage />} />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
