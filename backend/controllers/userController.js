61ffa63594be799225ee5447172fc0f0e85208a1
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-12 12:31:00 +0300

    feat: :sparkles: Add "getUserProfile" controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,6 +69,23 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid user data');
   }
 });
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
 export {
   loginUser,
