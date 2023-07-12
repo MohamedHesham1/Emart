1c5792f70ef85adf94458ec5ac9b14c002dedf56
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-12 11:51:01 +0300

    feat: :sparkles: Add "generateToken" function for JWT token generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
-import Jwt from 'jsonwebtoken';
+import { generateToken } from '../utils/generateToken.js';
+
 // @desc    loginUser user & get token
 // @route   POST /api/users/login
 // @access  Public
@@ -9,18 +10,9 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = Jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
+    generateToken(res, user._id);
 
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
-    });
-
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
