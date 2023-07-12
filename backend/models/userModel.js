c8ba4eba36c965a8bf594632c5035f2e03baff7e
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-12 11:59:30 +0300

    refactor: :recycle: use bcrypt for password hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,9 +27,20 @@ const userSchema = mongoose.Schema(
   }
 );
 
+// compare entered password with stored password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
