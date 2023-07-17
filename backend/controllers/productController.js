8912abc8fbac4ac647111ab6fde38ed68d3f0138
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-17 13:27:37 +0300

    feat: :sparkles: Add "orderController"

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,17 @@
 import Product from '../models/productModel.js';
 import asyncHandler from 'express-async-handler';
 
+// @desc    Get all products
+// @route   Get /api/products
+// @access  Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
 
+// @desc    Get a single product
+// @route   POST /api/product/:id
+// @access  Public
 const getProductsById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
