04fdecd9bcfd02522bc58d588b88ac0490002201
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-22 09:39:21 +0300

    fix: :bug: Correct "updateOrderToDelivered" code

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -97,14 +97,8 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id);
 
   if (order) {
-    order.isPaid = true;
-    order.paidAt = Date.now();
-    order.paymentResult = {
-      id: req.body.id,
-      status: req.body.status,
-      update_time: req.body.update_time,
-      email_address: req.body.payer.email_address,
-    };
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
 
     const updatedOrder = await order.save();
 
