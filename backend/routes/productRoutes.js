cfd9378492f074b8b6ed2872a53d9b6cd0935482
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-07-28 22:23:34 +0300

    feat: :sparkles: Add "createProductReview" route and controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import {
   createProduct,
+  createProductReview,
+  deleteProduct,
   getProducts,
   getProductsById,
   updateProduct,
-  deleteProduct,
 } from '../controllers/productController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+
 router
   .route('/:id')
   .get(getProductsById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
